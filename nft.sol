dfac7dfc7f9cac8479fe6fa4bd08e9baac16a8c7
ainuryaka <97097119+ainuryaka@users.noreply.github.com>
2023-10-23 17:02:50 +0600

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -11,3 +10,24 @@ contract MyNFT is ERC721 {
     uint256 public constant TOTAL_SUPPLY = 10000;
     mapping(uint256 => string) private _tokenURIs;
 
+constructor() ERC721("MyNFT", "MYN") {}
+
+    function mintNFT(address to, string memory tokenURI) public {
+        require(_tokenIds.current() < TOTAL_SUPPLY, "Total supply reached");
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(to, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        _tokenIds.increment();
+    }
+
+
+function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+}
